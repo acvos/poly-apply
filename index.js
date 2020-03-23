bedd0eaaf3da05e0f2fbc9c388734dfdf04f3540
Yair <yairopro@gmail.com>
2020-03-23 23:17:15 +0200

    tabs

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 const curry = require('curry');
 
 function isPromise(x) {
-  return (x instanceof Promise);
+	return (x instanceof Promise);
 }
 
 function apply(fn, to) {
-  // constant
-  if (!(fn instanceof Function)) {
-    return fn;
-  }
-  
-  // promised arguments
-  if (isPromise(to)) {
-    return to.then(function (xs) {
-      return apply(fn, xs);
-    });
-  }
-  
-  // accept one direct argument
-  if (!(to instanceof Array)){
-      return [to];
-  }
+	// constant
+ 	if (!(fn instanceof Function)) {
+		return fn;
+	}
 
-  // promise in arguments
-  if (to.find(isPromise)) {
-    return Promise.all(to).then(function (xs) {
-      return apply(fn, xs);
-    });
-  }
+	// promised arguments
+	if (isPromise(to)) {
+		return to.then(function (xs) {
+			return apply(fn, xs);
+		});
+	}
 
-  return fn.apply(undefined, to);
+	// accept one direct argument
+	if (!(to instanceof Array)){
+		return [to];
+	}
+
+	// promise in arguments
+	if (to.find(isPromise)) {
+		return Promise.all(to).then(function (xs) {
+			return apply(fn, xs);
+		});
+	}
+
+	return fn.apply(undefined, to);
 }
 
 module.exports = curry(apply);
